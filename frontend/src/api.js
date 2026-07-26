996e1439ffec52aadd6ca31085425b35e7ca6d15
Sivakumar10036 <rsiva19022005@gmail.com>
2026-07-26 10:02:13 +0530

    Connect frontend to Render backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-export default axios.create({
-    baseURL: "http://127.0.0.1:5000"
+const api = axios.create({
+    baseURL: "https://student-performance-analysis-yzgu.onrender.com"
 });
+
+export default api;
